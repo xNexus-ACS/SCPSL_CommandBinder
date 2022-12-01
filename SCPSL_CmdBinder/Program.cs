deb7306fd8537247c431bf594b98795e50cbc0b2
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-02 00:45:47 +0100

    Add files via upload

---
+++ SCPSL_CmdBinder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SCPSL_CmdBinder
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var gamePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SCP Secret Laboratory");
+
+            if (!Directory.Exists(gamePath))
+            {
+                Console.WriteLine("SCP Secret Laboratory in AppData / .config Not Found!");
+                
+                Task.Delay(5000).ContinueWith(_ => Environment.Exit(0));
+            }
+            var bindingsTxt = Path.Combine(gamePath, "cmdbinding.txt");
+            if (!File.Exists(bindingsTxt))
+                File.Create(bindingsTxt).Close();
+            
+            // TODO: Improve the code to correctly add the binding
+            try
+            {
+                Console.WriteLine("Enter Command: ");
+                var command = Console.ReadLine();
+                
+                if (command is not null)
+                {
+                    File.WriteAllText(bindingsTxt, $"{command}\n");
+                    Console.WriteLine("Command Added!");
+                    return;
+                }
+                Console.WriteLine("Command Null!");
+                Task.Delay(5000).ContinueWith(_ => Environment.Exit(0));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An Error occured while writing in the file: " + ex);
+            }
+        }
+    }
+}
---
+++ SCPSL_CmdBinder/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SCPSL_CmdBinder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SCPSL_CmdBinder")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F6C8495B-4C8B-4656-BE2E-C57BBAA3E411")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
